c6275b09c0ac2ab6b489ff5906521f4165f32f49
Mixlo <berggren.bjorn@hotmail.com>
2019-02-25 13:46:49 +0100

    Major changes, implemented communication platform, improved UI, other fixes.

---
+++ uugame.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from commplatform import StartMenu
+
+def main():
+    print("Welcome to UU-GAME!")
+    print()
+    StartMenu().show()
+    
+if __name__ == "__main__":
+    main()
